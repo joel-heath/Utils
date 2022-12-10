84ade29eef10f601bfbc1a44dae1214a5e2e356a
Joel Heath <joelheath@outlook.com>
2022-12-10 16:26:43 +0000

    Add project files.

---
+++ JH24Utils/Simplex.cs
@@ -0,0 +1,109 @@
+﻿namespace JH24Utils;
+
+public class Simplex
+{
+    public static decimal[][] OneStageSimplex(decimal[,] tableux)
+    {
+        bool maximised = false;
+        while (!maximised)
+        {
+            if (Iteration(tableux)) { maximised = true; }
+        }
+
+        return ToBasicJagArray(tableux);
+    }
+    static int TopRow(decimal[,] table)
+    {
+        // returns -1 if top row has no negatives (maximised)
+        (int col, decimal val) = (0, 0);
+        for (int i = 1; i < table.GetLength(1) - 1; i++)
+        {
+            if (table[0, i] < val) { col = i; val = table[0, i]; }
+        }
+
+        return val == 0 ? -1 : col;
+    }
+
+    static int RatioTest(decimal[,] table, int var)
+    {
+        (int row, decimal val) min = (0, decimal.MaxValue);
+        for (int i = 1; i < table.GetLength(0); i++)
+        {
+            if (table[i, var] != 0)
+            {
+                decimal val = table[i, table.GetLength(1) - 1] / table[i, var];
+                if (val > 0 && val < min.val) min = (i, val);
+            }
+        }
+
+        return min.row;
+    }
+
+    static bool Iteration(decimal[,] table)
+    {
+        // returns true if maximum value has been found
+        int chosenCol = TopRow(table);
+        if (chosenCol == -1) return true;
+
+        int chosenRow = RatioTest(table, chosenCol);
+
+        decimal dividend = table[chosenRow, chosenCol];
+
+        // divide chosen row first
+        for (int i = 0; i < table.GetLength(1); i++)
+        {
+            table[chosenRow, i] = table[chosenRow, i] / dividend;
+        }
+
+        // reduce all other values of variable to 0
+        for (int r = 0; r < table.GetLength(0); r++)
+        {
+            if (r == chosenRow) continue;
+            decimal scalar = -table[r, chosenCol];
+
+            for (int c = 0; c < table.GetLength(1); c++)
+            {
+                table[r, c] = table[r, c] + (scalar * table[chosenRow, c]);
+            }
+        }
+
+        return false;
+    }
+
+    static decimal[][] ToBasicJagArray(decimal[,] table)
+    {
+        // converts 2d array into jagged array, aswell as removing any non-basic columns
+        int rows = table.GetLength(0);
+        int cols = table.GetLength(1);
+
+        List<int> nonBasics = new();
+
+        for (int r = 0; r < rows; r++)
+        {
+            for (int c = 0; c < cols - 1; c++)
+            {
+                if (table[r, c] != 0 && table[r, c] != 1)
+                {
+                    if (!nonBasics.Contains(c)) nonBasics.Add(c);
+                }
+            }
+        }
+
+        decimal[][] output = new decimal[rows][];
+
+        for (int r = 0; r < rows; r++)
+        {
+            output[r] = new decimal[cols - nonBasics.Count];
+
+            // t is table index, c is output index 
+            for (int t = 0, c = 0; t < cols; t++)
+            {
+                if (nonBasics.Contains(t)) continue;
+
+                output[r][c++] = table[r, t];
+            }
+        }
+
+        return output;
+    }
+}
---
+++ JH24UtilsDemo/UtilsDemo.cs
@@ -0,0 +1,109 @@
+﻿namespace JH24UtilsDemo;
+
+internal class UtilsDemo
+{
+    static decimal ReadNum()
+    {
+        int reset = Console.CursorLeft; // for resetting newline from user input
+        string rawUInput = Console.ReadLine() ?? "0";
+        Console.CursorLeft = reset + rawUInput.Length;
+        Console.CursorTop--;
+
+        return decimal.Parse(rawUInput);
+    }
+
+    static void SimplexOneStage()
+    {
+        Console.WriteLine("The Simplex Algorithm");
+
+        Console.Write("Enter the variable names (seperated by a space): ");
+        string[] vars = (Console.ReadLine() ?? "x").Split(" ");
+
+        Console.Write("Enter the number of constraints: ");
+        int constCount = int.Parse(Console.ReadLine() ?? "1");
+
+        decimal[,] tableux = new decimal[constCount + 1, vars.Length + 2];
+
+        Console.WriteLine("(Enter each coefficient of each variable in the order that you entered the variables initially.)");
+        Console.WriteLine("Enter the coefficients of the objective function.");
+        Console.Write("P = ");
+        tableux[0, 0] = 1;
+
+        for (int i = 0; i < vars.Length; i++)
+        {
+            tableux[0, i + 1] = -ReadNum();
+            Console.Write($"{vars[i]}{(i == vars.Length - 1 ? "" : " + ")}");
+        }
+
+        Console.WriteLine("\nNow enter each constraint in the same way.");
+
+        for (int i = 1; i < tableux.GetLength(0); i++)
+        {
+            Console.Write($"Constraint {i}: ");
+            for (int j = 1; j < tableux.GetLength(1); j++)
+            {
+                tableux[i, j] = ReadNum();
+
+                if (j != vars.Length + 1)
+                {
+                    Console.Write($"{vars[j - 1]}{(j < vars.Length ? " + " : " = ")}");
+                }
+            }
+            Console.WriteLine();
+        }
+
+        decimal[][] final = JH24Utils.Simplex.OneStageSimplex(tableux);
+
+        string[] trueVars = new string[] { "P" }.Concat(vars).ToArray();
+
+        // if it were x + y = 5, Item1 would be {x, y}, Item2 would be 5
+        (string[], decimal)[] output = new (string[], decimal)[final.Length];
+
+        for (int r = 0; r < final.Length; r++)
+        {
+            string[] rowVars = final[r].SkipLast(1).Select((v, i) => (v, trueVars[i])).Where(t => t.v > 0.0000000001M || t.v < -0.0000000001M).Select(t => t.Item2).ToArray();
+
+            output[r] = (rowVars, final[r][^1]);
+        }
+
+        for (int i = 0; i < output.Length; i++)
+        {
+            Console.WriteLine($"{string.Join(" + ", output[i].Item1)} = {output[i].Item2:0.#####}");
+        }
+
+        Console.ReadKey(true);
+    }
+
+    static void ListFuncs()
+    {
+        Console.ForegroundColor = ConsoleColor.Gray;
+        for (int i = 0; i < funcs.Length; i++)
+        {
+            Console.WriteLine($"{i + 1}. {funcs[i]}");
+        }
+        Console.ForegroundColor = ConsoleColor.White;
+    }
+
+    static readonly string[] funcs = { "Simplex" };
+    static void Main()
+    {
+        Console.ForegroundColor = ConsoleColor.Gray;
+        Console.WriteLine("Enter 'exit' to exit, and 'list' to list functions.");
+        Console.ForegroundColor = ConsoleColor.White;
+        bool finished = false;
+        while (!finished)
+        {
+            Console.Write("Which algorithm would you like to try? ");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            string uInput = Console.ReadLine() ?? "Simplex";
+            Console.ForegroundColor = ConsoleColor.White;
+
+            switch (uInput.ToLower())
+            {
+                case "simplex": SimplexOneStage(); break;
+                case "list": ListFuncs(); break;
+                case "exit": finished = true; break;
+            }
+        }
+    }
+}
